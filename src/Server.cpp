3c06590c4cba391934574aa6be0f6b8ab47ee816
Paras Saini <pa17112002@gmail.com>
2025-02-18 17:45:25 +0000

    2 commands

--- src/Server.cpp
+++ src/Server.cpp
@@ -2,6 +2,7 @@
 #include <filesystem>
 #include <fstream>
 #include <string>
+#include <zlib.h>
 
 int main(int argc, char *argv[])
 {
@@ -13,7 +14,6 @@ int main(int argc, char *argv[])
     std::cerr << "Logs from your program will appear here!\n";
 
     // Uncomment this block to pass the first stage
-    //
     if (argc < 2) {
         std::cerr << "No command provided.\n";
         return EXIT_FAILURE;
@@ -41,7 +41,21 @@ int main(int argc, char *argv[])
             std::cerr << e.what() << '\n';
             return EXIT_FAILURE;
         }
-    } else {
+    }
+    else if (command =="cat-file")
+    {
+        if (argc < 4) {
+            std::cerr << "Invalid number of arguments, missing parameters: -p <hash> \n";
+            return EXIT_FAILURE;
+        }
+        const auto blob_hash = std::string_view(argv[3],40);
+        const auto blob_dir = blob_hash.substr(0,2);
+        const auto blob_name = blob_hash.substr(2);
+
+    }
+     
+    
+    else {
         std::cerr << "Unknown command " << command << '\n';
         return EXIT_FAILURE;
     }
